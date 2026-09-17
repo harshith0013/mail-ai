1353a9abc0ce2b869c06048382f5acb69e774a51
harshith <harshith@harshiths-MacBook-Neo.local>
2026-09-17 22:59:13 +0530

    Minimize Google OAuth Gmail scopes

--- backend/app/api/routes/google_auth.py
+++ backend/app/api/routes/google_auth.py
@@ -20,7 +20,6 @@ SCOPES = [
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/gmail.modify",
 ]
 
